f5d54b3552ea858c9ef183de511e928c044016fa
Loki Astari <Loki.Astari@gmail.com>
2026-05-05 21:58:11 -0700

    Add all the event handlers for NisseBolt
        Now users can add event handlers for all Events that are supported by ThorsSlack.

--- src/NisseBolt/App.h
+++ src/NisseBolt/App.h
@@ -19,9 +19,40 @@
 
 namespace ThorsAnvil::Nisse::Bolt
 {
+namespace Event = ThorsAnvil::Slack::Event;
+
+template<typename T>
+using EventHandler   = std::function<void(T const& event, Say const& say)>;
+using AnyEventHandler   = std::variant< EventHandler<Event::AppDeleted>, EventHandler<Event::AppHomeOpened>, EventHandler<Event::AppInstalled>, EventHandler<Event::AppRateLimited>, EventHandler<Event::AppRequested>, EventHandler<Event::AppUninstalledTeam>, EventHandler<Event::AppUninstalled>, EventHandler<Event::AppMentioned>,
+                                        EventHandler<Event::AssistantThreadContextChanged>, EventHandler<Event::AssistantThreadStarted>,
+                                        EventHandler<Event::CallRejected>,
+                                        EventHandler<Event::ChannelArchive>, EventHandler<Event::ChannelCreated>, EventHandler<Event::ChannelDeleted>, EventHandler<Event::ChannelHistoryChanged>, EventHandler<Event::ChannelIdChanged>, EventHandler<Event::ChannelLeft>, EventHandler<Event::ChannelPostingPermissions>, EventHandler<Event::ChannelRename>, EventHandler<Event::ChannelShared>, EventHandler<Event::ChannelUnshared>,
+                                        EventHandler<Event::DndUpdated>, EventHandler<Event::DndUpdatedUser>,
+                                        EventHandler<Event::EmailDomainChanged>,
+                                        EventHandler<Event::EmojiChanged>,
+                                        EventHandler<Event::EntityDetailsRequested>,
+                                        EventHandler<Event::FileChange>, EventHandler<Event::FileCommentAdded>, EventHandler<Event::FileCommentDeleted>, EventHandler<Event::FileCommentEdited>, EventHandler<Event::FileCreated>, EventHandler<Event::FileDeleted>, EventHandler<Event::FilePublic>, EventHandler<Event::FileShared>, EventHandler<Event::FileUnshared>,
+                                        EventHandler<Event::FunctionExecuted>,
+                                        EventHandler<Event::GridMigrationFinished>, EventHandler<Event::GridMigrationStarted>,
+                                        EventHandler<Event::GroupClose>, EventHandler<Event::GroupDeleted>, EventHandler<Event::GroupHistoryChanged>, EventHandler<Event::GroupLeft>, EventHandler<Event::GroupOpen>, EventHandler<Event::GroupRename>,
+                                        EventHandler<Event::ImClose>, EventHandler<Event::ImCreated>, EventHandler<Event::ImHistoryChanged>, EventHandler<Event::ImOpen>,
+                                        EventHandler<Event::InviteRequested>,
+                                        EventHandler<Event::LinkShared>,
+                                        EventHandler<Event::MemberJoinedChannel>, EventHandler<Event::MemberLeftChannel>,
+                                        // EventHandler<Event::Message>,
+                                        EventHandler<Event::MessageMetadataPosted>, EventHandler<Event::MessageMetadataUpdated>, EventHandler<Event::MessageMetadataDeleted>,
+                                        EventHandler<Event::PinAdded>, EventHandler<Event::PinRemoved>,
+                                        EventHandler<Event::ReactionAdded>, EventHandler<Event::ReactionRemoved>,
+                                        EventHandler<Event::SharedChannelInviteAccepted>, EventHandler<Event::SharedChannelInviteApproved>, EventHandler<Event::SharedChannelInviteDeclined>, EventHandler<Event::SharedChannelInviteReceived>, EventHandler<Event::SharedChannelInviteRequested>,
+                                        EventHandler<Event::StarAdded>, EventHandler<Event::StarRemoved>,
+                                        EventHandler<Event::SubteamCreated>, EventHandler<Event::SubteamMembersChanged>, EventHandler<Event::SubteamSelfAdded>, EventHandler<Event::SubteamSelfRemoved>, EventHandler<Event::SubteamUpdated>,
+                                        EventHandler<Event::TeamAccessGranted>, EventHandler<Event::TeamAccessRevoked>, EventHandler<Event::TeamDomainChange>, EventHandler<Event::TeamJoin>, EventHandler<Event::TeamRename>,
+                                        EventHandler<Event::TokensRevoked>,
+                                        EventHandler<Event::UserChange>, EventHandler<Event::UserConnection>, EventHandler<Event::UserHuddleChanged>
+                                    >;
 
 using Filter         = std::function<bool(ThorsAnvil::Slack::Event::Message const&)>;
-using MessageHandler = std::function<void(ThorsAnvil::Slack::Event::Message const&, Say const& say)>;
+using MessageHandler = EventHandler<ThorsAnvil::Slack::Event::Message>;
 
 class App: ThorsAnvil::ThorsMug::MugPluginSimple
 {
@@ -30,7 +61,8 @@ class App: ThorsAnvil::ThorsMug::MugPluginSimple
     ThorsAnvil::Slack::EventHandlerMap      eventHandlerMap;
     ThorsAnvil::Slack::SlackClient          client;
     ThorsAnvil::Slack::SlackEventHandler    slackHandler;
-    std::vector<std::pair<Filter, MessageHandler>> messageHandlers;
+    std::vector<std::pair<Filter, MessageHandler>>  messageHandlers;
+    std::vector<AnyEventHandler>                    eventHandlers;
 
     public:
         App(AppConfig const& config);
@@ -42,8 +74,14 @@ class App: ThorsAnvil::ThorsMug::MugPluginSimple
         void message(std::regex filter, MessageHandler&& handler);          // Simplish to use regular expression filter
         void message(MessageHandler&& handler);                             // No filter handle all events.
 
+        void event(EventHandler<Event::Message>&& handler)                  {message(std::move(handler));}
+        template<typename E>
+        void event(EventHandler<E>&& handler)                               {eventHandlers.emplace_back(std::move(handler));}
+
     private:
         // Handle incoming events and send to the registered handlers.
+        template<typename T>
+        void handleEvent(ThorsAnvil::Slack::EventRequest<T> const& request);
         void handleEventMessage(ThorsAnvil::Slack::EventRequest<ThorsAnvil::Slack::Event::Message> const& request);
 
 
